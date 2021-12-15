74b01bcea3f41ef8bcc8874ec185ef584a98536f
fyenne <fyenne@hotmail.com>
2021-12-15 18:36:57 +0800

    git push minus

--- fifo.py
+++ fifo.py
@@ -24,155 +24,204 @@ def run_etl(start_date, end_date ,env):
     """
     offline version
     """
-    sql = """
-        SELECT * FROM dm_dsc_smart.dwd_task
-        where substr(station_name, 1,4) = 'COAC' 
-        and inc_day between '""" + start_date + "' and '" + end_date + """'
-        """
+    sql = 
+    # -- select * from  dsc_dwd.dwd_wh_dsc_inventory_dtl_di 
+    # -- where src = 'scale'
+    # -- and data_source in ('scale_hpi', 'scale_michelin', 'scale_fas', 'scale_bose')
+    # -- and inc_day in ('20211124', '20211117', '20211110', '20211103', 
+    # -- '20211027', '20211020', '20211013','20211006')
+    # -- and usage_flag = '1'
     print(sql)
-    coach = spark.sql(sql).select("*").toPandas()
+    df = spark.sql(sql).select("*").toPandas()
     print("==================================read_table================================")
-    print(coach.head())
+    print(df.head())
+
+
+    def load_data(oucode):
+        """
+        load bose data;
+        所有类型的qty都要加起来哦
+        只选择有多个收货日期的货物
+        """
+        
+        df = df[df['ou_code'].astype(str) == oucode]
+        df ['recived_date'] = pd.to_datetime(df['recived_date'])
+        df = df[['wms_company_name', 'wms_warehouse_id','sku_code', 'sku_name', 'sku_desc', 'location',\
+            'lock_codes', 'on_hand_qty', 'in_transit_qty','allocated_qty', 'shelf_days', 
+            'recived_date','usage_flag', 'inc_day']]
+        # qty = pd.Series(df.columns).str.extract('(.+qty)').dropna(axis = 0)
+        # df['qty'] = df[qty[0]].sum(axis = 1).round(2)
+        df['qty'] = df['on_hand_qty']
+        
+        # bose_inv.drop_duplicates(subset = ['sku_code', 'recived_date', 'lock_codes', 'inc_day', 'qty'])
+        # 没有重复的 目前看....aaa
+        df = df.groupby(['recived_date', 'sku_code', 'lock_codes','inc_day', 'wms_warehouse_id'], dropna = False).agg(
+        {
+            'qty':sum,
+            'location': set
+        }
+        ).sort_values(['sku_code', 'recived_date']).reset_index()
+        # 只选择有多个收货日期的货物
+        filter0 = df.groupby(['sku_code'])['recived_date'].agg(
+        {
+            set
+        }
+            ).reset_index()
+
+        filter0 = pd.DataFrame(filter0[filter0['set'].apply(len)> 1]['sku_code'].drop_duplicates())
+        bose_inv = filter0.merge(df, on = ['sku_code'], how = 'inner')\
+            .sort_values(['recived_date','sku_code', 'inc_day'])
+        
+        return bose_inv
+
+    # bose_inv = load_data('BOSE_SH')
+    # for loop needed or use group by.
+
+    df0 = pd.DataFrame()
+    def snapshot():
+        """
+        pivot table. inc_day 快照 作为 cols
+        添加标记.
+        """
+        global df0, bose_inv
+        for i in bose_inv['sku_code'].unique(), ascii= False, colour = 'green':
+            df_out = bose_inv[bose_inv['sku_code'] == i]\
+                .pivot_table(columns=['inc_day'], index = 'recived_date', values=['qty']).reset_index()
+            df_out['sku_code'] = i
+            df0 = pd.concat([df0, df_out], axis = 0)
+        try:
+            df0.columns = df0.columns.get_level_values(level=1)
+        except:
+            pass
+        df0.columns = list(df0.columns[0:8]) + ['received_date','sku']
+        df0 = df0.sort_values(['sku', 'received_date'])
+        df0['mark'] = 0
+        
+        df0['mark'] = df0['mark'].where(df0.iloc[:, 0:7].isna().all(axis = 1) == False, 'new')
+        df0['mark'] = df0['mark'].where(~df0.iloc[:,7].isna() , 'clear')
+        # fill na~
+        df0 = df0.fillna(0)
+        bose_inv = bose_inv.rename({'sku_code':'sku', 'recived_date':'received_date'}, axis = 1)\
+            .reset_index(drop = True).drop(['inc_day', 'qty'], axis = 1)
+        bose_inv = bose_inv.drop_duplicates(subset = ['sku', 'received_date', 'lock_codes'])
+        df0 = df0.merge(bose_inv, on = ['sku','received_date'], how = 'left')
+
+        # may lock
+        df0['mark'] = df0['mark'].where(df0.iloc[:, 0:8].fillna(0).nunique(axis = 1) > 1, 'may_lock')
+    snapshot()
+
+
+    def err_part():
+        """
+        findout who are the naught peach.
+        err 中干掉了 new 干掉了maylock 
+        """
+        df_err = df0[df0['mark'] != 'new']
+        # 补充可能被锁的标记
+        # df_err['mark'] = df_err['mark'].where(df_err.iloc[:, 0:8].fillna(0).nunique(axis = 1) > 1, 'may_lock')
+        # 干掉了maylock
+        df_err = df_err[df_err['mark'] != 'may_lock'].sort_values(['sku', 'received_date'])
+        df_err['change'] = df_err.iloc[:,0:8].diff(axis = 1).sum(axis = 1)
+        shift = df_err.groupby(['sku', 'wms_warehouse_id']).shift(1) 
+        # q = []
+        # for i in shift['mark']:
+        #     if pd.isna(i):
+        #         q.append('clear')
+        #     elif i =='clear':
+        #         q.append('clear')
+        #     else:
+        #         q.append('check')
+        # shift['mark'] = q
+        # shift['mark2'] = shift.iloc[:,0:8].diff(axis = 1).sum(axis = 1)
+        shift = shift[['mark','change']]
+        shift.columns = ['lag_mark', 'lag_change']
+        shift['lag_mark'] = shift['lag_mark'].where(~shift['lag_mark'].isna(), 'clear')
+        df_err = pd.concat([df_err, shift], axis = 1)
+        # df_wrong = df_err[(df_err['lag_mark'] != 'clear') & (df_err['change'] != 0)]
+        # df_good  = df_err[(df_err['lag_mark'] != 'clear') | (df_err['change'] != 0)]
+        return df_err
+    df_err = err_part()
 
 
 
-def load_data(wms_warehouse_id):
-    """
-    load bose data;
-    所有类型的qty都要加起来哦
-    只选择有多个收货日期的货物
-    """
-    df = df9
-    df = df[df['wms_warehouse_id'].astype(str) == wms_warehouse_id]
-    df ['recived_date'] = pd.to_datetime(df['recived_date'])
-    df = df[['wms_company_name', 'wms_warehouse_id','sku_code', 'sku_name', 'sku_desc', 'location',\
-        'lock_codes', 'on_hand_qty', 'in_transit_qty','allocated_qty', 'shelf_days', 
-        'recived_date','usage_flag', 'inc_day']]
-    # qty = pd.Series(df.columns).str.extract('(.+qty)').dropna(axis = 0)
-    # df['qty'] = df[qty[0]].sum(axis = 1).round(2)
-    df['qty'] = df['on_hand_qty']
-    
-    # bose_inv.drop_duplicates(subset = ['sku_code', 'recived_date', 'lock_codes', 'inc_day', 'qty'])
-    # 没有重复的 目前看....aaa
-    df = df.groupby(['recived_date', 'sku_code', 'lock_codes','inc_day', 'wms_warehouse_id'], dropna = False).agg(
-    {
-        'qty':sum,
-        'location': set
-    }
-    ).sort_values(['sku_code', 'recived_date']).reset_index()
-    # 只选择有多个收货日期的货物
-    filter0 = df.groupby(['sku_code'])['recived_date'].agg(
-    {
-        set
-    }
-        ).reset_index()
-
-    filter0 = pd.DataFrame(filter0[filter0['set'].apply(len)> 1]['sku_code'].drop_duplicates())
-    bose_inv = filter0.merge(df, on = ['sku_code'], how = 'inner')\
-        .sort_values(['recived_date','sku_code', 'inc_day'])
-    
-    return bose_inv
-
-bose_inv = load_data('BOSE_SH')
-
-df0 = pd.DataFrame()
-def snapshot():
-    """
-    pivot table. inc_day 快照 作为 cols
-    添加标记.
-    """
-    global df0, bose_inv
-    for i in tqdm(bose_inv['sku_code'].unique(), ascii= False, colour = 'green'):
-        df_out = bose_inv[bose_inv['sku_code'] == i]\
-            .pivot_table(columns=['inc_day'], index = 'recived_date', values=['qty']).reset_index()
-        df_out['sku_code'] = i
-        df0 = pd.concat([df0, df_out], axis = 0)
-    try:
-        df0.columns = df0.columns.get_level_values(level=1)
-    except:
-        pass
-    df0.columns = list(df0.columns[0:8]) + ['received_date','sku']
-    df0 = df0.sort_values(['sku', 'received_date'])
-    df0['mark'] = 0
-    # 这个吊东`西不知道为什么是反选的 
-    df0['mark'] = df0['mark'].where(df0.iloc[:, 0:7].isna().all(axis = 1) == False, 'new')
-    df0['mark'] = df0['mark'].where(~df0.iloc[:,7].isna() , 'clear')
-    # fill na~
-    df0 = df0.fillna(0)
-    bose_inv = bose_inv.rename({'sku_code':'sku', 'recived_date':'received_date'}, axis = 1)\
-        .reset_index(drop = True).drop(['inc_day', 'qty'], axis = 1)
-    bose_inv = bose_inv.drop_duplicates(subset = ['sku', 'received_date', 'lock_codes'])
-    df0 = df0.merge(bose_inv, on = ['sku','received_date'], how = 'left')
-
-    # may lock
-    df0['mark'] = df0['mark'].where(df0.iloc[:, 0:8].fillna(0).nunique(axis = 1) > 1, 'may_lock')
-snapshot()
-
-
-def err_part():
-    """
-    findout who are the naught peach.
-    err 中干掉了 new 干掉了maylock 
-    """
-    df_err = df0[df0['mark'] != 'new']
-    # 补充可能被锁的标记
-    # df_err['mark'] = df_err['mark'].where(df_err.iloc[:, 0:8].fillna(0).nunique(axis = 1) > 1, 'may_lock')
-    # 干掉了maylock
-    df_err = df_err[df_err['mark'] != 'may_lock'].sort_values(['sku', 'received_date'])
-    df_err['change'] = df_err.iloc[:,0:8].diff(axis = 1).sum(axis = 1)
-    shift = df_err.groupby(['sku', 'wms_warehouse_id']).shift(1) 
-    # q = []
-    # for i in shift['mark']:
-    #     if pd.isna(i):
-    #         q.append('clear')
-    #     elif i =='clear':
-    #         q.append('clear')
-    #     else:
-    #         q.append('check')
-    # shift['mark'] = q
-    # shift['mark2'] = shift.iloc[:,0:8].diff(axis = 1).sum(axis = 1)
-    shift = shift[['mark','change']]
-    shift.columns = ['lag_mark', 'lag_change']
-    shift['lag_mark'] = shift['lag_mark'].where(~shift['lag_mark'].isna(), 'clear')
-    df_err = pd.concat([df_err, shift], axis = 1)
-    # df_wrong = df_err[(df_err['lag_mark'] != 'clear') & (df_err['change'] != 0)]
-    # df_good  = df_err[(df_err['lag_mark'] != 'clear') | (df_err['change'] != 0)]
-    return df_err
-df_err = err_part()
+    def output(df_err, df0):
+        dishes = list(df_err[(df_err['lag_mark'] != 'clear') \
+            & (df_err['change'] < 0)
+            & (df_err['change'] != df_err['lag_change'])]['sku'].unique())
+        return df0[df0['sku'].isin(dishes)]
+    def check(sku, df0):
+        return df0[(df0['lock_codes'] == '1000') & (df0['sku'].isin(sku))].sort_values(['sku','received_date'])
 
 
 
-def output(df_err, df0):
-    dishes = list(df_err[(df_err['lag_mark'] != 'clear') \
-        & (df_err['change'] < 0)
-        & (df_err['change'] != df_err['lag_change'])]['sku'].unique())
-    return df0[df0['sku'].isin(dishes)]
-def check(sku, df0):
-    return df0[(df0['lock_codes'] == '1000') & (df0['sku'].isin(sku))].sort_values(['sku','received_date'])
+    view = output(df_err_bose, df0_bose)
+    # 1000全新 3000退货 4000破损产品-
+    def ou_level_lock_codes(lock_code_to_eliminate):
+        """
+        正则. lock_code 需要被排除的, 依赖view表格. 
+        """
+        # output(df_err_bose,df0_bose)['sku'].unique()
+        select_none_lock  = pd.DataFrame(
+            view.groupby('sku')['mark'].apply(list).astype(str).str.match('.+may')
+            ).reset_index()
+        select_none_lock2 = pd.DataFrame(
+            view.groupby('sku')['lock_codes'].apply(list).astype(str).str.match('.+'+lock_code_to_eliminate)
+            ).reset_index()
+        # 去重    
+        bose_err_list = set(select_none_lock[~select_none_lock['mark']]['sku'].unique())
+        bose_err_list2 = list(select_none_lock2[~select_none_lock2['lock_codes']]['sku'].unique())
+        bose_err_list = list(bose_err_list.intersection(bose_err_list2))
+        bose_err_list = list(set(bose_err_list))
+        return bose_err_list
 
-
-
-view = output(df_err_bose,df0_bose)
-# 1000全新 3000退货 4000破损产品-
-def ou_level_lock_codes(lock_code_to_eliminate):
-    """
-    正则. lock_code 需要被排除的, 依赖view表格. 
-    """
-    # output(df_err_bose,df0_bose)['sku'].unique()
-    select_none_lock  = pd.DataFrame(
-        view.groupby('sku')['mark'].apply(list).astype(str).str.match('.+may')
-        ).reset_index()
-    select_none_lock2 = pd.DataFrame(
-        view.groupby('sku')['lock_codes'].apply(list).astype(str).str.match('.+'+lock_code_to_eliminate)
-        ).reset_index()
-    # 去重    
-    bose_err_list = set(select_none_lock[~select_none_lock['mark']]['sku'].unique())
-    bose_err_list2 = list(select_none_lock2[~select_none_lock2['lock_codes']]['sku'].unique())
-    bose_err_list = list(bose_err_list.intersection(bose_err_list2))
-    bose_err_list = list(set(bose_err_list))
-    return bose_err_list
-
-bose_err_list = ou_level_lock_codes('(4000|3000)')
-bose_definite_wrong = check(bose_err_list, df0_bose)
-bose_definite_wrong\
+    bose_err_list = ou_level_lock_codes('(4000|3000)')
+    bose_definite_wrong = check(bose_err_list, df0_bose)
+    bose_definite_wrong\
     # .to_csv('./data_up/bose_fifo.csv', index = None, encoding = 'utf_8_sig')
+
+
+    """
+    merge table preparation:
+    """
+   
+
+    # merge_table = "dsc_dws.dws_dsc_huawei_operation_sum_df"
+    # if env == 'dev':
+    #     merge_table = 'tmp_' + merge_table
+    # else:
+    #     pass
+    # print('看一下merge_table from john')
+    # print(merge_table)
+    
+    # inc_df = spark.sql("""select * from df""")
+    # print("===============================merge_table--%s================================="%merge_table)
+    # # merge_table = "tmp_dsc_dws.dws_dsc_huawei_operation_sum_df"
+    # # print(merge_table)
+    # print('{note:=>50}'.format(note=merge_table) + '{note:=>50}'.format(note=''))
+
+    # spark.sql("""set spark.hadoop.hive.exec.dynamic.partition.mode=nonstrict""")
+    # # (table_name, df, pk_cols, order_cols, partition_cols=None):
+    # merge_data = MergeDFToTable(merge_table, inc_df, \
+    #     "ou, update_date, inc_day", "inc_day", partition_cols="inc_day")
+    # merge_data.merge()
+
+
+
+def main():
+    args = argparse.ArgumentParser()
+    args.add_argument("--start_date", help="start date for refresh data, format: yyyyMMdd"
+                          , default=[(datetime.now()).strftime("%Y%m%d")], nargs="*")
+
+    args.add_argument("--env", help="dev environment or prod environment", default="dev", nargs="*")
+
+    args_parse = args.parse_args()
+    start_date = args_parse.start_date[0]
+    env = args_parse.env[0]
+ 
+    run_etl(start_date, env)
+
+    
+if __name__ == '__main__':
+    main()
+
+    
